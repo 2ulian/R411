49a62270054bd49b76a98ff096378e51f3346795
Julian <rayconstantyjulian@gmail.com>
2026-01-27 14:34:20 +0100

    Refactor MainActivity to implement navigation with HomeScreen and FormScreen

--- app/src/main/java/com/example/tp2navigationcompose/MainActivity.kt
+++ app/src/main/java/com/example/tp2navigationcompose/MainActivity.kt
@@ -4,13 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.tp2navigationcompose.ui.theme.TP2NavigationComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,11 +29,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TP2NavigationComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                MaterialTheme {
+                    AppNavigation()
                 }
             }
         }
@@ -31,17 +38,66 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun AppNavigation() {
+    // contrôleur navigation
+    val navController = rememberNavController()
+
+    // Définir le système de navigation
+    NavHost(navController = navController, startDestination = "home") {
+        composable(route = "home") {
+            HomeScreen(navController = navController)
+        }
+        composable(route = "form") {
+            FormScreen(navController = navController)
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun AppNavigationPreview() {
     TP2NavigationComposeTheme {
-        Greeting("Android")
+        AppNavigation()
     }
+}
+
+@Composable
+fun HomeScreen(navController: androidx.navigation.NavHostController) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Bienvenue dans ma première application compose navigation",
+            style = MaterialTheme.typography.titleMedium
+        )
+        Spacer(modifier = Modifier.height(24.dp))
+        Button(onClick = { navController.navigate(route = "form") }) {
+            Text(text = "Accéder au formulaire")
+        }
+    }
+}
+
+@Composable
+fun FormScreen(navController: androidx.navigation.NavHostController) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Page du formulaire.",
+            style = MaterialTheme.typography.titleMedium
+        )
+        Spacer(modifier = Modifier.height(24.dp))
+        Button(onClick = { navController.popBackStack() }) {
+            Text(text = "Retour")
+        }
+    }
+}
 }
